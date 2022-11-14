19cc0c5fea35bd994f50c608ea6b545a5c588c06
seungchan <chanchu@hansung.ac.kr>
2022-11-15 01:34:12 +0900

    [#6] Image

---
+++ FogFog-iOS/FogFog-iOS/Resources/Image/FogImage.swift
@@ -0,0 +1,18 @@
+//
+//  FogImage.swift
+//  FogFog-iOS
+//
+//  Created by 김승찬 on 2022/11/15.
+//
+
+import UIKit.UIImage
+
+
+public enum FogImage {
+
+    static let logo = UIImage(named: "ic_fogfog")!
+    static let hamburger = UIImage(named: "ic_ham")!
+    static let location = UIImage(named: "ic_location")!
+    static let minus = UIImage(named: "ic_minus")!
+    static let plus = UIImage(named: "ic_plus")!
+}
