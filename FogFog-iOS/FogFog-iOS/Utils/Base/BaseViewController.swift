174bd2613f40db6ce6b198d87f535fdf30a266ef
seungchan <chanchu@hansung.ac.kr>
2022-11-14 20:17:01 +0900

    [#3] Utils

---
+++ FogFog-iOS/FogFog-iOS/Utils/Base/BaseViewController.swift
@@ -0,0 +1,25 @@
+//
+//  BaseViewController.swift
+//  FogFog-iOS
+//
+//  Created by 김승찬 on 2022/11/14.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setStyle()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    func setStyle() {}
+}
