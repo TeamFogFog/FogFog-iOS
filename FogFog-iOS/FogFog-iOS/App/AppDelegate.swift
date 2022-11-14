0ea3254e62773906858116a2aad1256252d64957
seungchan <chanchu@hansung.ac.kr>
2022-11-14 20:16:02 +0900

    [#3] App

---
+++ FogFog-iOS/FogFog-iOS/App/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  FogFog-iOS
+//
+//  Created by 김승찬 on 2022/11/14.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
