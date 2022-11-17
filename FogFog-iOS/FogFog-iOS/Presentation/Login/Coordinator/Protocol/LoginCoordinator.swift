477fd1e7336d38c54aa54d72d1d753bb50d94b4a
seungchan <chanchu@hansung.ac.kr>
2022-11-17 23:28:44 +0900

    [#12] LoginCoordinator

---
+++ FogFog-iOS/FogFog-iOS/Presentation/Login/Coordinator/Protocol/LoginCoordinator.swift
@@ -0,0 +1,14 @@
+//
+//  LoginCoordinator.swift
+//  FogFog-iOS
+//
+//  Created by 김승찬 on 2022/11/17.
+//
+
+import Foundation
+
+protocol LoginCoordinator: Coordinator {
+    
+    func showLoginViewController()
+    func connectMapCoordinator()
+}
