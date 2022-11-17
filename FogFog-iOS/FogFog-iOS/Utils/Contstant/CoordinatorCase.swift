d95a19b6375bf7b9459691dc6a77e0dad6c940ac
seungchan <chanchu@hansung.ac.kr>
2022-11-17 23:29:47 +0900

    [#12] CoordinatorCase

---
+++ FogFog-iOS/FogFog-iOS/Utils/Contstant/CoordinatorCase.swift
@@ -0,0 +1,14 @@
+//
+//  CoordinatorCase.swift
+//  FogFog-iOS
+//
+//  Created by 김승찬 on 2022/11/17.
+//
+
+import Foundation
+
+enum CoordinatorCase {
+    case app
+    case login
+    case map
+}
