1e5eec15c36aa936eef36b2997065ecbe2e77ffe
eunjuchoi <eunjuzzoo@naver.com>
2022-11-20 16:30:32 +0900

    [#17] ViewModelType 생성

---
+++ FogFog-iOS/FogFog-iOS/Presentation/Common/Protocol/ViewModelType.swift
@@ -0,0 +1,21 @@
+//
+//  ViewModelType.swift
+//  FogFog-iOS
+//
+//  Created by EUNJU on 2022/11/20.
+//
+
+import Foundation
+
+import RxSwift
+
+protocol ViewModelType {
+
+    associatedtype Input
+    associatedtype Output
+
+    var disposeBag: DisposeBag { get set }
+
+    func transform(input: Input) -> Output
+    
+}
