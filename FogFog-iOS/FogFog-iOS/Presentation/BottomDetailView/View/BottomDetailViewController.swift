a4115ec63ef3224225e851909103a982b1b05e98
김태현 <taehyk528@gmail.com>
2022-11-20 16:12:13 +0900

    [#18] BottomDetailView 파일 추가

--- FogFog-iOS/FogFog-iOS/Presentation/BottomDetailView/View/BottomDetailViewController.swift
+++ FogFog-iOS/FogFog-iOS/Presentation/BottomDetailView/View/BottomDetailViewController.swift
@@ -7,11 +7,9 @@
 
 import UIKit
 
-class BottomDetailViewController: UIViewController {
+final class BottomDetailViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 }
---
+++ FogFog-iOS/FogFog-iOS/Presentation/BottomDetailView/ViewModel/BottomDetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  BottomDetailViewModel.swift
+//  FogFog-iOS
+//
+//  Created by taekki on 2022/11/20.
+//
+
+import Foundation
+
+final class BottomDetailViewModel {
+    
+}
