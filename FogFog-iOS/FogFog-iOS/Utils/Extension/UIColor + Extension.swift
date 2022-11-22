e97b04ebafe921e4434d774d938701063793a43e
eunjuchoi <eunjuzzoo@naver.com>
2022-11-23 00:02:52 +0900

    [#17] 컬러셋 추가

--- FogFog-iOS/FogFog-iOS/Utils/Extension/UIColor + Extension.swift
+++ FogFog-iOS/FogFog-iOS/Utils/Extension/UIColor + Extension.swift
@@ -10,22 +10,91 @@ import UIKit.UIColor
 extension UIColor {
     
     @nonobjc class var fogBlue: UIColor {
-        return UIColor(red: 91/255, green: 157/255, blue: 255/255, alpha: 1.0)
-    }
-    
-    @nonobjc class var fogBlack: UIColor {
-        return UIColor(red: 21/255, green: 22/255, blue: 24/255.0, alpha: 1.0)
-    }
-    
-    @nonobjc class var gray1: UIColor {
-        return UIColor(red: 68/255, green: 69/255, blue: 71/255.0, alpha: 1.0)
+        return UIColor(red: 91.0 / 255.0 , green: 157.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
     }
     
     @nonobjc class var shadowGray: UIColor {
-        return UIColor(red: 28/255, green: 76/255, blue: 200/255.0, alpha: 1.0)
+        return UIColor(red: 28.0 / 255.0, green: 76.0 / 255.0, blue: 200.0 / 255.0, alpha: 1.0)
     }
     
-    @nonobjc class var kakaoYellow: UIColor {
-        return UIColor(red: 255/255, green: 219/255, blue: 30/255.0, alpha: 1.0)
+    @nonobjc class var grayBlack: UIColor {
+        return UIColor(red: 21.0 / 255.0, green: 22.0 / 255.0, blue: 24.0 / 255.0, alpha: 1.0)
     }
+    
+    @nonobjc class var grayGray7: UIColor {
+        return UIColor(red: 185.0 / 255.0, green: 185.0 / 255.0, blue: 186.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var grayGray3: UIColor {
+        return UIColor(red: 91.0 / 255.0, green: 92.0 / 255.0, blue: 94.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var grayGray8: UIColor {
+        return UIColor(red: 208.0 / 255.0, green: 208.0 / 255.0, blue: 209.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var grayWhite: UIColor {
+        return UIColor(white: 1.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var grayGray4: UIColor {
+        return UIColor(red: 115.0 / 255.0, green: 115.0 / 255.0, blue: 117.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var grayGray9: UIColor {
+        return UIColor(white: 232.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var grayGray2: UIColor {
+        return UIColor(red: 68.0 / 255.0, green: 69.0 / 255.0, blue: 71.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var grayGray5: UIColor {
+        return UIColor(red: 138.0 / 255.0, green: 138.0 / 255.0, blue: 140.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var grayGray10: UIColor {
+        return UIColor(white: 243.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var mainBlueMain2: UIColor {
+        return UIColor(red: 79.0 / 255.0, green: 139.0 / 255.0, blue: 1.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var mainBlue2: UIColor {
+        return UIColor(red: 55.0 / 255.0, green: 94.0 / 255.0, blue: 153.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var mainBlue4: UIColor {
+        return UIColor(red: 124.0 / 255.0, green: 177.0 / 255.0, blue: 1.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var mainBlue1: UIColor {
+        return UIColor(red: 36.0 / 255.0, green: 63.0 / 255.0, blue: 102.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var mainBlue5: UIColor {
+        return UIColor(red: 157.0 / 255.0, green: 196.0 / 255.0, blue: 1.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var mainBlue9: UIColor {
+        return UIColor(red: 245.0 / 255.0, green: 249.0 / 255.0, blue: 1.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var opacityGray1: UIColor {
+        return UIColor(red: 21.0 / 255.0, green: 22.0 / 255.0, blue: 24.0 / 255.0, alpha: 0.5)
+    }
+    
+    @nonobjc class var opacityGray2: UIColor {
+        return UIColor(red: 21.0 / 255.0, green: 22.0 / 255.0, blue: 24.0 / 255.0, alpha: 0.65)
+    }
+    
+    @nonobjc class var etcRed: UIColor {
+        return UIColor(red: 251.0 / 255.0, green: 109.0 / 255.0, blue: 104.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var etcYellow: UIColor {
+        return UIColor(red: 1.0, green: 219.0 / 255.0, blue: 30.0 / 255.0, alpha: 1.0)
+    }
+
 }
