3bd4c1e1ce649ca42bb69a4fa79f1f217da63272
eunjuchoi <eunjuzzoo@naver.com>
2022-11-25 19:59:18 +0900

    [#22] UITextField+ 메서드 추가

--- FogFog-iOS/FogFog-iOS/Utils/Extension/UITextField + Extension.swift
+++ FogFog-iOS/FogFog-iOS/Utils/Extension/UITextField + Extension.swift
@@ -22,4 +22,24 @@ extension UITextField {
         self.rightView = paddingView
         self.rightViewMode = .always
     }
+    
+    /// clearButton 설정 메서드
+    func setClearButton() {
+        let clearButton = UIButton(type: .custom)
+        clearButton.setImage(FogImage.deleteTextIcon, for: .normal)
+        clearButton.frame = CGRect(x: 0, y: 0, width: 18, height: 18)
+        clearButton.contentMode = .scaleAspectFit
+        clearButton.addTarget(self, action: #selector(UITextField.clear(sender:)), for: .touchUpInside)
+        
+        let rightView = UIView(frame: CGRect(x: 0, y: 0, width: 34, height: 18))
+        rightView.addSubview(clearButton)
+        
+        self.rightView = rightView
+        self.rightViewMode = .whileEditing
+    }
+    
+    @objc
+    private func clear(sender: AnyObject) {
+        self.text = ""
+    }
 }
