5fa777c485b8af209412ac8df30abdb3cada0662
김태현 <taehyk528@gmail.com>
2022-12-03 17:47:21 +0900

    [#28] SplashViewController 계층 재생성

---
+++ FogFog-iOS/FogFog-iOS/Presentation/Splash/View/SplashViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SplashViewController.swift
+//  FogFog-iOS
+//
+//  Created by taekki on 2022/12/03.
+//
+
+import UIKit
+
+final class SplashViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
