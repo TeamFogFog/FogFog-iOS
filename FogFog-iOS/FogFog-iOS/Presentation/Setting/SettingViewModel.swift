a9271a10d33b876e1b30b5d5e289ccccf56a8c9b
eunjuchoi <eunjuzzoo@naver.com>
2022-12-23 00:17:50 +0900

    [#29] SettingViewModel 파일 추가

---
+++ FogFog-iOS/FogFog-iOS/Presentation/Setting/SettingViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  SettingViewModel.swift
+//  FogFog-iOS
+//
+//  Created by EUNJU on 2022/12/22.
+//
+
+import Foundation
+
+import RxCocoa
+import RxSwift
+
+final class SettingViewModel: ViewModelType {
+    
+    var disposeBag = DisposeBag()
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+    
+    }
+    
+    func transform(input: Input) -> Output {
+        let output = Output()
+        
+        return output
+    }
+}
