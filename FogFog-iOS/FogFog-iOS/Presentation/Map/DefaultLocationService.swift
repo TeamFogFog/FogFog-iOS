a87e7d13b8837bbd3e2b84451d18446b352ca17a
seungchan <chanchu@hansung.ac.kr>
2023-03-26 17:42:38 +0900

    [#19] CoreLocation 설정

---
+++ FogFog-iOS/FogFog-iOS/Presentation/Map/DefaultLocationService.swift
@@ -0,0 +1,70 @@
+//
+//  DefaultLocationService.swift
+//  FogFog-iOS
+//
+//  Created by 김승찬 on 2023/03/26.
+//
+
+import CoreLocation
+import Foundation
+
+import RxSwift
+import RxRelay
+
+final class DefaultLocationService: NSObject, LocationService {
+    
+    var locationManager: CLLocationManager?
+    var disposeBag: DisposeBag = DisposeBag()
+    var authorizationStatus = BehaviorRelay<CLAuthorizationStatus>(value: .notDetermined)
+    
+    override init() {
+        super.init()
+        self.locationManager = CLLocationManager()
+        self.locationManager?.distanceFilter = CLLocationDistance(3)
+        self.locationManager?.delegate = self
+        self.locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func start() {
+        
+        self.locationManager?.startUpdatingLocation()
+    }
+    
+    func stop() {
+        
+        self.locationManager?.stopUpdatingLocation()
+    }
+    
+    func requestAuthorization() {
+        
+        self.locationManager?.requestWhenInUseAuthorization()
+    }
+    
+    func observeUpdatedAuthorization() -> Observable<CLAuthorizationStatus> {
+        
+        return self.authorizationStatus.asObservable()
+    }
+    
+    func observeUpdatedLocation() -> Observable<[CLLocation]> {
+        
+        return PublishRelay<[CLLocation]>.create({ emitter in
+            self.rx.methodInvoked(#selector(CLLocationManagerDelegate.locationManager(_:didUpdateLocations:)))
+                .compactMap({ $0.last as? [CLLocation] })
+                .subscribe(onNext: { location in
+                    emitter.onNext(location)
+                })
+                .disposed(by: self.disposeBag)
+            return Disposables.create()
+        })
+    }
+}
+
+extension DefaultLocationService: CLLocationManagerDelegate {
+    func locationManager(
+        _ manager: CLLocationManager,
+        didUpdateLocations locations: [CLLocation]) {}
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        self.authorizationStatus.accept(status)
+    }
+}
