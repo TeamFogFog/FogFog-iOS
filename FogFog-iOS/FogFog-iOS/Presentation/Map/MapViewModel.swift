1e81a2d521f0d7e3648c9a137ab4feccbdb7d27f
seungchan <chanchu@hansung.ac.kr>
2023-03-26 17:43:25 +0900

    [#19] 위치 권한 로직 추가

--- FogFog-iOS/FogFog-iOS/Presentation/Map/MapViewModel.swift
+++ FogFog-iOS/FogFog-iOS/Presentation/Map/MapViewModel.swift
@@ -7,31 +7,52 @@
 
 import UIKit
 
+import CoreLocation
 import RxSwift
 import RxCocoa
 
+@frozen
+enum LocationAuthorizationStatus {
+    case allowed, disallowed, notDetermined
+}
+
 final class MapViewModel: ViewModelType {
     
+    var authorizationStatus = BehaviorSubject<LocationAuthorizationStatus?>(value: nil)
+    var userLocation = PublishSubject<CLLocation>()
     var disposeBag = DisposeBag()
     
     private weak var coordinator: MapCoordinator?
+    private let locationService: LocationService
     
-    init(coordinator: MapCoordinator?) {
+    init(coordinator: MapCoordinator?, locationService: LocationService) {
         self.coordinator = coordinator
+        self.locationService = locationService
     }
     
     struct Input {
+        let viewDidLoad: Observable<Void>
         let tapMenuButton: Signal<Void>
         let tapBlurEffectView: Signal<Void>
     }
     
     struct Output {
         let isVisible: Driver<Bool>
+        let currentUserLocation: Driver<CLLocationCoordinate2D>
     }
-        
+    
     func transform(input: Input) -> Output {
         
         let sideBarState = PublishRelay<Bool>()
+        let currentUserLocation = PublishRelay<CLLocationCoordinate2D>()
+        
+        input.viewDidLoad
+            .withUnretained(self)
+            .subscribe(onNext: { owner, _ in
+                owner.checkAuthorization()
+                owner.observeUserLocation()
+            })
+            .disposed(by: disposeBag)
         
         input.tapMenuButton
             .emit(onNext: { _ in
@@ -45,6 +66,47 @@ final class MapViewModel: ViewModelType {
             })
             .disposed(by: disposeBag)
         
-        return Output(isVisible: sideBarState.asDriver(onErrorJustReturn: false))
+        self.userLocation
+            .map({ $0.coordinate })
+            .bind(to: currentUserLocation)
+            .disposed(by: disposeBag)
+        
+        return Output(isVisible: sideBarState.asDriver(onErrorJustReturn: false),
+                      currentUserLocation: currentUserLocation.asDriver(onErrorJustReturn: CLLocationCoordinate2D(latitude: 20, longitude: 20)))
+    }
+}
+
+private extension MapViewModel {
+    
+    func checkAuthorization() {
+        
+        self.locationService.observeUpdatedAuthorization()
+            .withUnretained(self)
+            .subscribe(onNext: { owner, status in
+                switch status {
+                case .authorizedAlways, .authorizedWhenInUse:
+                    owner.authorizationStatus.onNext(.allowed)
+                    owner.locationService.start()
+                case .notDetermined:
+                    owner.authorizationStatus.onNext(.notDetermined)
+                    owner.locationService.requestAuthorization()
+                case .denied, .restricted:
+                    owner.authorizationStatus.onNext(.disallowed)
+                @unknown default:
+                    owner.authorizationStatus.onNext(nil)
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    func observeUserLocation() {
+        return self.locationService.observeUpdatedLocation()
+            .compactMap({ $0.last })
+            .withUnretained(self)
+            .subscribe(onNext: { owner, location in
+                owner.userLocation.onNext(location)
+                print("location", location)
+            })
+            .disposed(by: self.disposeBag)
     }
 }
