11b29d2ef931a81d9332a820cac3156158287b32
김태현 <taehyk528@gmail.com>
2023-04-20 00:19:10 +0900

    [#41] SmokePlace 데이터 모델 수정
    - CodingKeys를 추가하였습니다.
    - init(from decoder: Decoder)를 직접 정의하여 예기치 못한 데이터 오류에 대응하였습니다.

--- FogFog-iOS/FogFog-iOS/Models/Place/SmokePlace.swift
+++ FogFog-iOS/FogFog-iOS/Models/Place/SmokePlace.swift
@@ -5,16 +5,26 @@
 //  Created by taekki on 2022/11/20.
 //
 
-import UIKit
+import Foundation
 
-/// 흡연구역에 대한 상세 정보
-/// - placeImage: 흡연구역 이미지(서버에서 받아온 url 변환해서 사용)
-/// - distance: 현재 위치 기준으로 떨어진 거리
-/// - placeTitle: 흡연구역명
-/// - placeAddress: 흡연구역 상세주소
-struct SmokePlace {
-    let placeImage: UIImage
-    let distance: Int
-    let placeTitle: String
-    let placeAddress: String
+struct SmokePlace: Decodable {
+    let name: String
+    let address: String
+    let imageURLString: String
+    let distance: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case address
+        case imageURLString = "image"
+        case distance
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.address = try container.decode(String.self, forKey: .address)
+        self.imageURLString = try container.decodeIfPresent(String.self, forKey: .imageURLString) ?? "NO IMAGE"
+        self.distance = try container.decode(String.self, forKey: .distance)
+    }
 }
