6874f723c790dad3e0fe9fc4cb9374996a307bc6
김태현 <taehyk528@gmail.com>
2023-04-20 17:28:27 +0900

    [#41] Message 데이터 모델 추가
    - 토스트 메시지에 사용할 데이터 모델입니다.

---
+++ FogFog-iOS/FogFog-iOS/Models/Message/MessageModel.swift
@@ -0,0 +1,13 @@
+//
+//  MessageModel.swift
+//  FogFog-iOS
+//
+//  Created by taekki on 2023/04/20.
+//
+
+import Foundation
+
+// 토스트 메시지에 사용할 데이터
+struct MessageModel {
+    let contents: String
+}
