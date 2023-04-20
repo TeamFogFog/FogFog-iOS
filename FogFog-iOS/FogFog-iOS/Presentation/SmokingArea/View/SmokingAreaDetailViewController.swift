59fcb8431d5eb7de30600fcad644afd733220119
김태현 <taehyk528@gmail.com>
2023-04-20 20:46:16 +0900

    [#41] SmokingAreaDetailViewController 바텀업 인터랙션 구현
    - 인자로 들어오는 Contents와 Presentable한 뷰의 타입에 따라서 하단에서 올라오는 뷰를 달리합니다.

--- FogFog-iOS/FogFog-iOS/Presentation/SmokingArea/View/SmokingAreaDetailViewController.swift
+++ FogFog-iOS/FogFog-iOS/Presentation/SmokingArea/View/SmokingAreaDetailViewController.swift
@@ -10,34 +10,82 @@ import UIKit
 import SnapKit
 import Then
 
-final class SmokingAreaDetailViewController: BaseViewController {
+class SmokingAreaDetailViewController: BaseViewController {
     
     // MARK: Properties
-    private let rootView = SmokeAreaDetailView()
-    private weak var viewModel: MapViewModel?
+    private let contents: any Contents
+    private let contentsView: any Presentable
     
     // MARK: Initializers
-    init(viewModel: MapViewModel?) {
-        self.viewModel = viewModel
+    init(
+        contents: any Contents,
+        contentsView: any Presentable
+    ) {
+        self.contents = contents
+        self.contentsView = contentsView
+        contentsView.bind(contents)
+
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    // MARK: Override Methods
-    override func loadView() {
-        self.view = rootView
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+
+    override func setStyle() {
+        super.setStyle()
         
-        rootView.showBottomSheet()
+        view.backgroundColor = .clear
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        contentsView.hide(withMovement: -contentsView.viewHeight(), withDuration: 0.3)
+        dismiss(animated: false)
+    }
+    
+    override func setLayout() {
+        super.setLayout()
+
+        guard let contentsView = contentsView as? UIView else { return }
+        view.addSubview(contentsView)
+        
+        contentsView.snp.makeConstraints {
+            $0.directionalHorizontalEdges.equalToSuperview().inset(13.adjusted)
+            $0.bottom.equalToSuperview().inset(20.adjustedH)
+        }
+    }
+
+    func show() {
+        let viewHeight = contentsView.viewHeight()
+        let bottomMargin = 49.adjustedH
+        contentsView.show(withMovement: viewHeight + bottomMargin, withDuration: 0.3)
+    }
+}
+
+enum BottomViewType {
+    case cardView
+    case messageView
+    
+    var view: any Presentable {
+        switch self {
+        case .cardView:
+            return SmokingAreaCardView()
+        
+        case .messageView:
+            return SmokingAreaMessageView()
+        }
+    }
+}
+
+extension UIViewController {
+    
+    func showBottomView(_ withContents: some Contents, _ viewType: BottomViewType) {
+        let viewController = SmokingAreaDetailViewController(contents: withContents, contentsView: viewType.view)
+        viewController.modalPresentationStyle = .overFullScreen
+        viewController.view.backgroundColor = .clear
+        present(viewController, animated: false) {
+            viewController.show()
+        }
     }
 }
