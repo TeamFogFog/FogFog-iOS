0412dfb793869740b0eaff77867b91ca9f88497b
김태현 <taehyk528@gmail.com>
2023-04-25 21:15:12 +0900

    [#41] BottomView 관련 메소드 기능 분리
    - BottomView를  show, hide, add하는 기능을 extension으로 분리했습니다.
    - Composition을 통해 좀 더 편하게 확장 및 수정이 가능합니다.

--- FogFog-iOS/FogFog-iOS/Utils/Extension/UIViewController + Extension.swift
+++ FogFog-iOS/FogFog-iOS/Utils/Extension/UIViewController + Extension.swift
@@ -19,13 +19,70 @@ extension UIViewController {
 }
 
 extension UIViewController {
+
+    /// Bottom Presentable View 추가
+    func addBottomView(_ subview: UIView, belowSubview: UIView? = nil) {
+        // 기존에 깔려있는 하위 뷰 제거
+        hideBottomView()
+
+        // 특정 하위 뷰 아래에 추가 또는 가장 하위 뷰로 추가
+        if let belowSubview = belowSubview {
+            view.insertSubview(subview, belowSubview: belowSubview)
+        } else {
+            view.addSubview(subview)
+        }
+        
+        // 레이아웃 초기화
+        let defaultBottomMargin = 20
+        let sideMargin = 13.adjusted
+        subview.snp.makeConstraints {
+            $0.bottom.equalToSuperview().offset(defaultBottomMargin)
+            $0.directionalHorizontalEdges.equalToSuperview().inset(sideMargin)
+        }
+    }
     
-    func showBottomView(_ withContents: some Contents, _ viewType: BottomViewType) {
-        let viewController = SmokingAreaDetailViewController(contents: withContents, contentsView: viewType.view)
-        viewController.modalPresentationStyle = .overFullScreen
-        viewController.view.backgroundColor = .clear
-        present(viewController, animated: false) {
-            viewController.show()
+    func hideBottomView() {
+        // 기존에 깔려있는 BottomView hide animation 및 제거
+        if let bottomView = view.subviews.first(where: { return ($0 is Presentable) }) {
+            bottomView.snp.updateConstraints {
+                $0.bottom.equalToSuperview().offset(20)
+            }
+
+            UIView.animate(withDuration: 0.3, animations: view.layoutIfNeeded) { _ in
+                bottomView.removeFromSuperview()
+            }
+        }
+    }
+
+    // 흡연 구역 카드 뷰 보여주기 메소드
+    func showCardView(
+        _ content: SmokingAreaResponseModel,
+        belowSubview: UIView? = nil,
+        withDuration: TimeInterval = 0.5
+    ) {
+        let cardView = SmokingAreaCardView()
+        cardView.bind(content)
+        addBottomView(cardView, belowSubview: belowSubview)
+
+        let bottomMargin = 49.adjustedH
+        DispatchQueue.main.async {
+            cardView.show(withMovement: cardView.viewHeight() + bottomMargin, withDuration: withDuration)
+        }
+    }
+    
+    // 흡연 구역 메시지 뷰(= 토스트 뷰) 보여주기 메소드
+    func showMessageView(
+        _ content: MessageModel,
+        belowSubview: UIView? = nil,
+        withDuration: TimeInterval = 0.5
+    ) {
+        let messageView = SmokingAreaMessageView()
+        messageView.bind(content)
+        addBottomView(messageView, belowSubview: belowSubview)
+
+        let bottomMargin = 49.adjustedH
+        DispatchQueue.main.async {
+            messageView.show(withMovement: messageView.viewHeight() + bottomMargin, withDuration: withDuration)
         }
     }
 }
