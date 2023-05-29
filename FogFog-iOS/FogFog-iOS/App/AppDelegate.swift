509f6b79b97f6e020fc36f6431c7f775c2735d51
김태현 <taehyk528@gmail.com>
2023-05-29 17:57:04 +0900

    [#49] Kakao SDK 초기화

--- FogFog-iOS/FogFog-iOS/App/AppDelegate.swift
+++ FogFog-iOS/FogFog-iOS/App/AppDelegate.swift
@@ -8,14 +8,19 @@
 import UIKit
 
 import GoogleMaps
+import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
+        
+        // MARK: - 구글 맵 설정
         GMSServices.provideAPIKey(Bundle.main.apiKey)
+        
+        // MARK: - 카카오 SDK 초기화
+        KakaoSDK.initSDK(appKey: Config.kakaoNativeAppKey)
         return true
     }
 }
