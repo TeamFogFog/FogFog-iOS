e1ef60f6b5f534c18ced2211ef33234371bd3fd1
eunjuchoi <eunjuzzoo@naver.com>
2023-05-31 15:42:50 +0900

    [#46] UserDefaults + Extension 파일 추가

---
+++ FogFog-iOS/FogFog-iOS/Utils/Extension/UserDefaults + Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UserDefaults + Extension.swift
+//  FogFog-iOS
+//
+//  Created by EUNJU on 2023/05/31.
+//
+
+import Foundation
+
+extension UserDefaults {
+    
+    enum Keys {
+        static let nickname = "nickname"
+    }
+}
