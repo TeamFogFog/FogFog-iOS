9c5abe85719f13d8fc9827ca7c6576c7da58be79
eunjuchoi <eunjuzzoo@naver.com>
2023-06-04 15:06:20 +0900

    [#46] property wrapper로 감싼 UserDefault 구조체 생성

---
+++ FogFog-iOS/FogFog-iOS/Utils/Contstant/UserDefault.swift
@@ -0,0 +1,23 @@
+//
+//  UserDefault.swift
+//  FogFog-iOS
+//
+//  Created by EUNJU on 2023/06/04.
+//
+
+import Foundation
+
+@propertyWrapper
+struct UserDefault<T> {
+    
+    let key: String
+    
+    init(key: String) {
+        self.key = key
+    }
+    
+    var wrappedValue: T? {
+        get { UserDefaults.standard.object(forKey: key) as? T }
+        set { UserDefaults.standard.set(newValue, forKey: key) }
+    }
+}
