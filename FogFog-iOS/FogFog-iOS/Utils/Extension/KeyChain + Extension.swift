bd935ed1e7ef45594ab4396905917d4f126efe3c
eunjuchoi <eunjuzzoo@naver.com>
2023-07-28 14:58:26 +0900

    [#59] 키체인 서비스 설정
    - 키체인 데이터를 관리하기 위한 메서드 정의
    - 데이터 관리에 용이하도록 키 값 설정

---
+++ FogFog-iOS/FogFog-iOS/Utils/Extension/KeyChain + Extension.swift
@@ -0,0 +1,17 @@
+//
+//  KeyChain + Extension.swift
+//  FogFog-iOS
+//
+//  Created by EUNJU on 2023/07/28.
+//
+
+import Foundation
+
+extension KeyChain {
+    
+    enum Keys {
+        static var accessToken = "accessToken"
+        static var refreshToken = "refreshToken"
+        static var socialType = "socialType"
+    }
+}
