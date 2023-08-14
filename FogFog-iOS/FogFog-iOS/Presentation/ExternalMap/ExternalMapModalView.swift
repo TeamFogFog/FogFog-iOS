5f85a89b4cca222a680276f9444d3fbc3312b6e9
김태현 <taehyk528@gmail.com>
2023-08-14 17:17:16 +0900

    [#56] Refactor ModalView using FlexLayout, PinLayout
    - ExternalMapModalView

--- FogFog-iOS/FogFog-iOS/Presentation/ExternalMap/ExternalMapModalView.swift
+++ FogFog-iOS/FogFog-iOS/Presentation/ExternalMap/ExternalMapModalView.swift
@@ -7,166 +7,161 @@
 
 import UIKit
 
+import FlexLayout
+import PinLayout
+
 final class ExternalMapModalView: BaseView {
+  
+  // MARK: - UI
+  
+  private let rootFlexContainer: UIView = {
+    let view = UIView()
+    view.clipsToBounds = false
+    view.backgroundColor = .black.withAlphaComponent(0.5)
+    return view
+  }()
+  
+  private let containerView: UIView = {
+    let view = UIView()
+    view.clipsToBounds = false
+    view.backgroundColor = .white
+    view.makeRounded(cornerRadius: 25)
+    return view
+  }()
+  
+  private let bubbleImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = FogImage.speechBubbleBig
+    imageView.contentMode = .scaleAspectFit
+    return imageView
+  }()
+  
+  private let popupIconImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = FogImage.popupMap
+    imageView.contentMode = .scaleAspectFit
+    imageView.addShadow(
+        offset: .init(width: 0, height: 5),
+        color: .fogBlue,
+        opacity: 0.3,
+        radius: 10
+    )
+    return imageView
+  }()
+ 
+  private let closeButton: UIView = {
+    let button = UIButton()
+    button.setImage(FogImage.btnX, for: .normal)
+    button.contentMode = .scaleAspectFit
+    return button
+  }()
+  
+  private let titleLabel: UILabel = {
+    let label = UILabel()
+    label.text = "연결할 지도 선택이 필요해요"
+    label.textColor = .black
+    label.font = .pretendardB(18)
+    return label
+  }()
+  
+  private let kakaoMapButton: FogButton = {
+    let button = FogButton(style: .selected)
+    button.title = ExternalMapType.kakao.title
+    button.makeRounded(cornerRadius: 12.adjusted)
+    return button
+  }()
+  
+  private let googleMapButton: FogButton = {
+    let button = FogButton(style: .unselected)
+    button.title = ExternalMapType.google.title
+    button.makeRounded(cornerRadius: 12.adjusted)
+    return button
+  }()
+  
+  private let naverMapButton: FogButton = {
+    let button = FogButton(style: .unselected)
+    button.title = ExternalMapType.naver.title
+    button.makeRounded(cornerRadius: 12.adjusted)
+    return button
+  }()
+  
+  private let confirmButton: FogButton = {
+    let button = FogButton(style: .normal)
+    button.title = "확인"
+    return button
+  }()
+  
+  override func setLayout() {
+    addSubview(rootFlexContainer)
+    addSubview(bubbleImageView)
+    addSubview(popupIconImageView)
     
-    // MARK: UI
-    let dimmedView = UIView()
-    let containerView = UIView()
-    let closeButton = UIButton()
-    let bubbleImageView = UIImageView()
-    let popupIconImageView = UIImageView()
-    let titleLabel = UILabel()
-    let buttonStack = UIStackView()
-    let lineView = UIView()
-    let confirmButton = FogButton(style: .normal)
+    rootFlexContainer.flex
+      .justifyContent(.center)
+      .alignItems(.center)
+      .define { flex in
+        flex.addItem(containerView)
+          .justifyContent(.start)
+          .alignItems(.center)
+          
+          .define { flex in
+            flex.addItem(closeButton).position(.absolute).start(16).top(16)
+            flex.addItem(titleLabel).marginTop(59.adjustedH)
+            
+            flex.addItem()
+              .define { flex in
+                flex.addItem(kakaoMapButton).height(54.adjustedH).marginTop(29.adjustedH)
+                flex.addItem(googleMapButton).height(54.adjustedH).marginTop(8.adjustedH)
+                flex.addItem(naverMapButton).height(54.adjustedH).marginTop(8.adjustedH)
+              }
+              .width(100%)
+              .paddingHorizontal(13.adjusted)
+            
+            flex.addItem().grow(1)
+            flex.addItem(confirmButton).width(100%).height(65.adjustedH)
+          }
+          .width(343.adjusted)
+          .height(375.adjustedH)
+      }
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
     
-    // MARK: Properties
-    private let padding: CGFloat = 20
-    private let containerRadius: CGFloat = 25
-    private let buttonRadius: CGFloat = 12
+    rootFlexContainer.pin.all()
+    rootFlexContainer.flex.layout()
     
-    // MARK: Init
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setStackView()
-    }
-    
-    // MARK: Setup UI
-    override func setStyle() {
-        super.setStyle()
-        
-        backgroundColor = .systemBackground
-        
-        dimmedView.do {
-            $0.backgroundColor = .grayGray2
-            $0.alpha = 0.65
-        }
-
-        containerView.do {
-            $0.backgroundColor = .white
-            $0.makeRounded(cornerRadius: containerRadius)
-        }
-        
-        closeButton.do {
-            $0.setImage(FogImage.btnX, for: .normal)
-        }
-        
-        popupIconImageView.do {
-            $0.image = FogImage.popupMap
-            $0.addShadow(
-                offset: .init(width: 0, height: 5),
-                color: .fogBlue,
-                opacity: 0.3,
-                radius: 10
-            )
-        }
-        
-        bubbleImageView.do {
-            $0.image = FogImage.speechBubbleBig
-        }
-        
-        titleLabel.do {
-            $0.text = "연결할 지도 선택이 필요해요"
-            $0.textColor = .black
-            $0.font = .pretendardB(18)
-        }
-        
-        buttonStack.do {
-            $0.axis = .vertical
-            $0.distribution = .fillEqually
-        }
-        
-        lineView.do {
-            $0.backgroundColor = .grayGray9
-        }
-        
-        confirmButton.do {
-            $0.title = "확인"
-            $0.makeRounded(cornerRadius: buttonRadius)
-        }
-        
-        buttonStack.do {
-            $0.axis = .vertical
-            $0.distribution = .fillEqually
-            $0.spacing = 8
-        }
-    }
-    
-    override func setLayout() {
-        addSubviews([dimmedView, containerView])
-        addSubviews([closeButton, popupIconImageView, bubbleImageView])
-        addSubviews([titleLabel, buttonStack, lineView, confirmButton])
-        
-        dimmedView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-        }
-        
-        containerView.snp.makeConstraints {
-            $0.centerY.equalToSuperview()
-            $0.directionalHorizontalEdges.equalToSuperview().inset(padding)
-            $0.height.greaterThanOrEqualTo(380)
-        }
-        
-        closeButton.snp.makeConstraints {
-            $0.top.leading.equalTo(containerView).inset(15)
-        }
-        
-        popupIconImageView.snp.makeConstraints {
-            $0.centerX.equalTo(containerView)
-            $0.centerY.equalTo(containerView.snp.top)
-            $0.size.equalTo(78)
-        }
-        
-        bubbleImageView.snp.makeConstraints {
-            $0.centerX.equalTo(popupIconImageView)
-            $0.bottom.equalTo(popupIconImageView.snp.top).offset(-6)
-        }
-        
-        titleLabel.snp.makeConstraints {
-            $0.top.equalTo(popupIconImageView.snp.bottom).offset(24)
-            $0.centerX.equalTo(containerView)
-        }
-        
-        confirmButton.snp.makeConstraints {
-            $0.bottom.equalTo(containerView.snp.bottom).inset(13)
-            $0.directionalHorizontalEdges.equalTo(containerView).inset(12)
-            $0.height.equalTo(54)
-        }
-        
-        lineView.snp.makeConstraints {
-            $0.directionalHorizontalEdges.equalTo(containerView)
-            $0.height.equalTo(1)
-            $0.bottom.equalTo(confirmButton.snp.top).offset(-12.5)
-        }
-        
-        buttonStack.snp.makeConstraints {
-            $0.top.equalTo(titleLabel.snp.bottom).offset(28)
-            $0.directionalHorizontalEdges.equalTo(containerView).inset(12)
-            $0.bottom.equalTo(lineView.snp.top).offset(-12.5)
-        }
-    }
+    popupIconImageView.pin.center(to: containerView.anchor.topCenter).size(78.adjustedH)
+    bubbleImageView.pin.bottomCenter(to: popupIconImageView.anchor.topCenter).width(217.adjusted).height(57.adjustedH).marginBottom(7.adjustedH)
+  }
 }
 
-// MARK: Private Methods
-extension ExternalMapModalView {
-    
-    private func setStackView() {
-        ExternalMapType.allCases.forEach { type in
-            let button = FogButton(style: .unselected)
-            button.title = type.title
-            button.makeRounded(cornerRadius: buttonRadius.adjusted)
-            buttonStack.addArrangedSubview(button)
-        }
-    }
+#if DEBUG
+import SwiftUI
+
+struct ViewPreviewProvider: UIViewRepresentable {
+  let view: UIView
+  
+  init(_ builder: () -> UIView) {
+    self.view = builder()
+  }
+  
+  func makeUIView(context: Context) -> some UIView {
+    return view
+  }
+  
+  func updateUIView(_ view: UIViewType, context: Context) {
+    view.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+    view.setContentHuggingPriority(.defaultHigh, for: .vertical)
+  }
 }
 
-// MARK: Public Methods
-extension ExternalMapModalView {
-
-    func select(to type: ExternalMapType) {
-        for (index, button) in buttonStack.arrangedSubviews.enumerated() {
-            guard let button = button as? FogButton else { return }
-            button.style = index == type.rawValue ? .selected : .unselected
-        }
+struct View_Previews: PreviewProvider {
+  static var previews: some View {
+    ViewPreviewProvider {
+      ExternalMapModalView()
     }
+    .previewLayout(.sizeThatFits)
+  }
 }
+#endif
--- FogFog-iOS/FogFog-iOS/Presentation/ExternalMap/ExternalMapModalViewController.swift
+++ FogFog-iOS/FogFog-iOS/Presentation/ExternalMap/ExternalMapModalViewController.swift
@@ -30,46 +30,5 @@ final class ExternalMapModalViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        bind()
-    }
-}
-
-extension ExternalMapModalViewController {
-    
-    // MARK: - Bind
-    func bind() {
-        guard
-            let kakaoButton = rootView.buttonStack.arrangedSubviews[0].subviews.first(where: { $0 is UIButton }) as? UIButton,
-            let googleButton = rootView.buttonStack.arrangedSubviews[1].subviews.first(where: { $0 is UIButton }) as? UIButton,
-            let naverButton = rootView.buttonStack.arrangedSubviews[2].subviews.first(where: { $0 is UIButton }) as? UIButton
-        else { return }
-
-        guard let viewModel = viewModel as? ExternalMapModalViewModel else { return }
-        let input = ExternalMapModalViewModel.Input(
-            kakaoButtonTrigger: kakaoButton.rx.tap,
-            googleButtonTrigger: googleButton.rx.tap,
-            naverButtonTrigger: naverButton.rx.tap,
-            confirmButtonTrigger: rootView.confirmButton.button.rx.tap,
-            closeButtonTrigger: rootView.closeButton.rx.tap
-        )
-        let output = viewModel.transform(input: input)
-        
-        output.didSelectMap
-            .bind(with: self) { viewController, mapType in
-                viewController.rootView.select(to: mapType)
-            }
-            .disposed(by: disposeBag)
-        
-        output.didConfirm
-            .bind(with: self) { viewController, _ in
-                viewController.dismiss(animated: true)
-            }
-            .disposed(by: disposeBag)
-        
-        output.didCancel
-            .bind(with: self) { viewController, _ in
-                viewController.dismiss(animated: true)
-            }
-            .disposed(by: disposeBag)
     }
 }
