3e4fecfa3b0b1cd5e34891c73c759fdc5bfe529b
eunjuchoi <eunjuzzoo@naver.com>
2023-08-24 12:07:09 +0900

    [#54] ReissueAPIService 클래스 싱글톤 패턴 제거
    - 객체 생성 후 메서드 호출하는 방식으로 수정

--- FogFog-iOS/FogFog-iOS/Networking/Foundation/AuthInterceptor.swift
+++ FogFog-iOS/FogFog-iOS/Networking/Foundation/AuthInterceptor.swift
@@ -13,6 +13,7 @@ import RxSwift
 final class AuthInterceptor: RequestInterceptor {
     
     private let disposeBag = DisposeBag()
+    private let reissueApiService = ReissueAPIService()
  
     func adapt(_ urlRequest: URLRequest,
                for session: Session,
@@ -44,7 +45,7 @@ final class AuthInterceptor: RequestInterceptor {
         }
         
         // 토큰 재발급 요청
-        ReissueAPIService.shared.reissueAuthentication()
+        reissueApiService.reissueAuthentication()
             .subscribe(onSuccess: { result in
                 Keychain.create(key: Keychain.Keys.accessToken, data: result?.accessToken ?? "")
                 Keychain.create(key: Keychain.Keys.refreshToken, data: result?.refreshToken ?? "")
