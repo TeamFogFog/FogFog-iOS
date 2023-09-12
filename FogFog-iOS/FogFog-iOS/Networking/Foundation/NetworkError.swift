380017f82a7464b2e4fb0278222823fae3604dcd
eunjuchoi <eunjuzzoo@naver.com>
2023-09-13 00:16:48 +0900

    [#69] NetworkError 케이스 추가
    - duplicated 케이스 추가

--- FogFog-iOS/FogFog-iOS/Networking/Foundation/NetworkError.swift
+++ FogFog-iOS/FogFog-iOS/Networking/Foundation/NetworkError.swift
@@ -13,5 +13,6 @@ enum NetworkError: Int {
     case unauthorized   = 401   // 소셜 로그인 토큰이 없거나 유효하지 않은 경우
     case forbidden      = 403   // 요청 id와 accessToken 정보가 매칭되지 않는 경우
     case notFound       = 404   // 유효한 유저 정보가 없는 경우
+    case duplicated     = 409   // 중복된 닉네임일 경우
     case serverError    = 500   // Internal Server Error
 }
