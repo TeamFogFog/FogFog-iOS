0c5316505dce3a40c2e124eaa50e0c9049c3a55f
seungchan <chanchu@hansung.ac.kr>
2023-11-21 23:30:05 +0900

    [#62] UIImage+

---
+++ FogFog-iOS/FogFog-iOS/Utils/Extension/UIImage + Extension.swift
@@ -0,0 +1,21 @@
+//
+//  UIImage + Extension.swift
+//  FogFog-iOS
+//
+//  Created by 김승찬 on 2023/10/24.
+//
+
+import UIKit.UIImage
+
+extension UIImage {
+  func resizedImage(sizeImage: CGSize) -> UIImage? {
+    let frame = CGRect(origin: CGPoint.zero, size: CGSize(width: sizeImage.width, height: sizeImage.height))
+    UIGraphicsBeginImageContextWithOptions(frame.size, false, 0)
+    draw(in: frame)
+    let resizedImage: UIImage? = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    withRenderingMode(.alwaysOriginal)
+    return resizedImage
+  }
+}
+
