d5bf316b13f215d617fa45c17e440bbb20e2d879
seungchan <chanchu@hansung.ac.kr>
2023-11-21 23:30:43 +0900

    [#62] MapCoordinator

--- FogFog-iOS/FogFog-iOS/Presentation/Map/DefaultLoactionService.swift
+++ FogFog-iOS/FogFog-iOS/Presentation/Map/DefaultLoactionService.swift
@@ -12,10 +12,11 @@ import RxSwift
 import RxRelay
 
 final class DefaultLocationService: NSObject, LocationService {
-    
+   
     var locationManager: CLLocationManager?
     var disposeBag: DisposeBag = DisposeBag()
     var authorizationStatus = BehaviorRelay<CLAuthorizationStatus>(value: .notDetermined)
+    var location = BehaviorRelay<[CLLocation]>(value: [CLLocation(latitude: 0, longitude: 0)])
     
     override init() {
         super.init()
@@ -57,9 +58,16 @@ final class DefaultLocationService: NSObject, LocationService {
 extension DefaultLocationService: CLLocationManagerDelegate {
     func locationManager(
         _ manager: CLLocationManager,
-        didUpdateLocations locations: [CLLocation]) {}
+        didUpdateLocations locations: [CLLocation]) {
+            if let location = locations.first {
+                print(self.location.value)
+            }
+            self.location.accept(locations)
+
+        }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         self.authorizationStatus.accept(status)
     }
 }
+
