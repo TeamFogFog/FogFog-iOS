6a9a91c5ce814d3e54074ae39d0601392ae227ad
seungchan <chanchu@hansung.ac.kr>
2023-11-21 23:30:28 +0900

    [#62] MapViewController

--- FogFog-iOS/FogFog-iOS/Presentation/Map/MapViewController.swift
+++ FogFog-iOS/FogFog-iOS/Presentation/Map/MapViewController.swift
@@ -18,15 +18,27 @@ final class MapViewController: BaseViewController {
     
     // MARK: Properties
     private lazy var navigationView = NavigationView()
+    private var markersArray: [GMSMarker] = []
+    private lazy var researchButton = UIButton()
     private lazy var sideBarView = SideBarView()
     private var blurEffectView: UIVisualEffectView!
-   
+    private let camera = GMSCameraPosition(latitude: 37.54330366639085, longitude: 127.04455548501139, zoom: 16)
+    private var currentLocation = BehaviorRelay(value: CLLocationCoordinate2D(latitude: 0, longitude: 0))
     private var mapView = GMSMapView()
     private var myMarker = GMSMarker()
-    
     private var viewModel: MapViewModel
     private let tapBlurEffectView = PublishRelay<Void>()
     private let disposeBag = DisposeBag()
+    private let researchButtonTapRelay = PublishRelay<CLLocationCoordinate2D>()
+
+    lazy var input = MapViewModel.Input(
+        viewDidLoad: self.rx.viewDidLoad,
+        viewWillAppear: self.rx.viewWillAppear,
+        tapMenuButton: navigationView.rx.menuButtonTapped,
+        tapBlurEffectView: tapBlurEffectView.asSignal(),
+        tapSettingButton: sideBarView.rx.settingButtonTapped,
+        tapResearchButton: researchButtonTapRelay.asObservable())
+    lazy var output = viewModel.transform(input: input)
     
     // MARK: Init
     init(viewModel: MapViewModel) {
@@ -40,13 +52,11 @@ final class MapViewController: BaseViewController {
     
     // MARK: Life Cycle
     override func loadView() {
-        self.view = mapView
+        self.makeMap()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        makeMarker()
         bind()
     }
     
@@ -62,15 +72,28 @@ final class MapViewController: BaseViewController {
         blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.alpha = 0.6
         blurEffectView.isHidden = true
+        
+        researchButton.do {
+            $0.setTitle("현 지도에서 재검색", for: .normal)
+            $0.backgroundColor = .fogBlue
+            $0.titleLabel?.font = .pretendardL(15)
+        }
     }
     
     override func setLayout() {
-        view.addSubviews([navigationView, blurEffectView, sideBarView])
-        
+        view.addSubviews([navigationView, blurEffectView, sideBarView, researchButton])
+
         navigationView.snp.makeConstraints {
             $0.top.equalToSuperview()
             $0.height.equalTo(102)
             $0.leading.trailing.equalToSuperview()
+        }
+        
+        researchButton.snp.makeConstraints {
+            $0.top.equalTo(navigationView.snp.bottom).offset(10)
+            $0.width.equalTo(138.adjusted)
+            $0.height.equalTo(40.adjustedH)
+            $0.centerX.equalToSuperview()
         }
         
         blurEffectView.snp.makeConstraints {
@@ -88,14 +111,33 @@ final class MapViewController: BaseViewController {
 // MARK: - Bind
 extension MapViewController {
     
+    private func makeMap() {
+        let mapID = GMSMapID(identifier: "42619687bc36aafd")
+        output.currentUserLocation
+            .asDriver()
+            .drive { coordinates in
+                self.currentLocation.accept(coordinates)
+                self.move(at: coordinates)
+            }
+            .disposed(by: disposeBag)
+        
+        mapView = GMSMapView(frame: .zero, mapID: mapID, camera: GMSCameraPosition(latitude: currentLocation.value.latitude, longitude: currentLocation.value.longitude, zoom: 16))
+
+        self.view = mapView
+    }
+    
     private func bind() {
-        let input = MapViewModel.Input(
-            viewDidLoad: self.rx.viewDidLoad,
-            viewWillAppear: self.rx.viewWillAppear,
-            tapMenuButton: navigationView.rx.menuButtonTapped,
-            tapBlurEffectView: tapBlurEffectView.asSignal(),
-            tapSettingButton: sideBarView.rx.settingButtonTapped)
-        let output = viewModel.transform(input: input)
+        researchButton.rx.tap
+            .map { CLLocationCoordinate2D(latitude: self.mapView.camera.target.latitude,
+                                          longitude: self.mapView.camera.target.longitude)}
+            .bind(to: researchButtonTapRelay)
+            .disposed(by: disposeBag)
+
+        researchButton.rx.tap
+            .bind(onNext: { _ in
+                self.removeMarkers()
+            })
+            .disposed(by: disposeBag)
         
         output.isVisible
             .asDriver()
@@ -113,13 +155,35 @@ extension MapViewController {
                 self.sideBarView.setNickname(result)
             })
             .disposed(by: disposeBag)
-        
+
         output.currentUserLocation
             .asDriver()
             .drive { coordinates in
-                self.move(at: coordinates)
+                self.currentLocation.accept(coordinates)
             }
             .disposed(by: disposeBag)
+        
+        output.smokingAreas
+            .drive(onNext: { coords in
+                for coord in coords {
+                    let latitude = coord.latitude
+                    let longitude = coord.longitude
+                    let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                    self.makeMarker(at: location)
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        output.smokingAreasCount
+            .drive(onNext: { count in
+                let toastView = FogToast()
+                let message = count == 0 ? "주변에 흡역구역이 없습니다." : "주변에 \(count)개의 흡연구역이 있습니다."
+                toastView.setContents(message)
+                toastView.present(on: self)
+                toastView.dismiss()
+            })
+            .disposed(by: disposeBag)
+        
     }
 }
 // MARK: - Custom Methods
@@ -154,13 +218,23 @@ private extension MapViewController {
         mapView.isMyLocationEnabled = true
     }
     
-    func makeMarker() {
-        let mapCenter = CLLocationCoordinate2DMake(37.57039821, 126.98914393)
+    func makeMarker(at coordinate: CLLocationCoordinate2D) {
+        let mapCenter = CLLocationCoordinate2DMake(coordinate.latitude, coordinate.longitude)
         let marker = GMSMarker(position: mapCenter)
-        marker.icon = FogImage.placeMarker
+        let image = FogImage.pinActive.resizedImage(sizeImage: CGSize(width: 40.adjusted, height: 40.adjustedH))
+        marker.icon = image
         marker.map = mapView
+        markersArray.append(marker)
     }
     
+    func removeMarkers() {
+        for marker in markersArray {
+            marker.map = nil
+        }
+        
+        markersArray.removeAll()
+    }
+
     func distance(from: CLLocationCoordinate2D) -> CLLocationDistance {
         let from = CLLocation(latitude: from.latitude, longitude: from.longitude)
         let to = CLLocation(latitude: 37.54330366639082, longitude: 127.04455548501139)
