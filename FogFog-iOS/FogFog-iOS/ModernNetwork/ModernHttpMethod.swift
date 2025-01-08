3f8fe4808062f305dabc24fff2051352086868b5
seungchan2 <chanchu@hansung.ac.kr>
2025-01-08 15:54:45 +0900

    feat: HTTPMethod 정의

---
+++ FogFog-iOS/FogFog-iOS/ModernNetwork/ModernHttpMethod.swift
@@ -0,0 +1,15 @@
+//
+//  ModernHttpMethod.swift
+//  FogFog-iOS
+//
+//  Created by MEGA_Mac on 1/8/25.
+//
+
+import Foundation
+
+enum ModernHttpMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
